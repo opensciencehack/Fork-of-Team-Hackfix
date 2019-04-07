e5728f13f7d8b44fe8a65929160a218601b05dd8
William Eklund <cedsson@gmail.com>
2019-04-07 07:19:46 +0200

    Fixade

---
+++ ObjectDetection/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Alturos.Yolo;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
+
+namespace ObjectDetection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var configurationDetector = new ConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                var items = yoloWrapper.Detect("image.png");
+            }
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg||";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Image.Source = new BitmapImage(new System.Uri(openFileDialog.FileName));
+            }
+        }
+    }
+}
